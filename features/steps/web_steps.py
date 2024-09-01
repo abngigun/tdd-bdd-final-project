2d0fc41570a2ab6b22e415d50ebb04b8db321473
abngigun <blossomson@gmail.com>
2024-09-01 07:10:11 -0400

    update file

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -104,8 +104,31 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
 
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    assert(found)
+
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    assert(name not in element.text)
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    # Use WebDriverWait to wait for the specified message to be present in the element with the ID 'flash_message'
+    # Use the expected_conditions.text_to_be_present_in_element method to check if the provided message is present in the text content of the element.
+    # Use the assert(found) statement to verify that the message was found in the flash message area.
+    
 ##################################################################
 # This code works because of the following naming convention:
 # The id field for text input in the html is the element name
